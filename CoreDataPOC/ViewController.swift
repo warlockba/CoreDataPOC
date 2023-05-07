0010e30ff357ea97ee64efdef546aa2740edec67
Andrei Botoaca <andrei.botoaca@gmail.com>
2023-05-07 14:37:31 +0200

    Initial Commit

---
+++ CoreDataPOC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataPOC
+//
+//  Created by Andrei Botoaca on 07.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
